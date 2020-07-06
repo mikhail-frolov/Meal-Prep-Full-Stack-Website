abeffc9edc4a86fb44ef611c9d996bcbd68247ef
Mikhail <mishafrolov882000@gmail.com>
2020-07-06 16:06:01 -0400

    Added Validation for Login Page

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const exphbs = require('express-handlebars');
-
+const bodyParser = require('body-parser');
 const meals = require("./model/meals");
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // set handlebars as template engine
 app.engine('handlebars', exphbs());
@@ -47,6 +49,28 @@ app.get("/login", (req, res) => {
     res.render("login", {
         title: "Login Page"
     });
+
+});
+
+app.post('/login', (req, res) => {
+
+    const errors = [];
+    if (req.body.username == "") {
+        errors.push("Username is required");
+    }
+
+    if (req.body.psw == "") {
+        errors.push("Password is required");
+    }
+
+    if (errors.length > 0) {
+        res.render("login", {
+            title: "Login Page",
+            errorMessages: errors
+        });
+    } else {
+        res.redirect("/");
+    }
 
 });
 
