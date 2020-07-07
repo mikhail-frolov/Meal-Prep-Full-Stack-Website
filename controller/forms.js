3d398536728676c1c584b9e49c5b5cbea3fb4782
Mikhail <mishafrolov882000@gmail.com>
2020-07-06 22:16:44 -0400

    Fixed login

--- controller/forms.js
+++ controller/forms.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 
 
@@ -80,5 +80,66 @@ router.post('/registration', (req, res) => {
 });
 
 
+
+module.exports = router;
+*/
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("Register", {
+        title: "Register",
+    });
+});
+
+router.get("/registration", (req, res) => {
+    res.render("Register", {
+        title: "Register"
+    });
+});
+
+router.post("/registration", (req, res) => {
+
+    const errors = [];
+    //Only letters and numbers, length 6-12
+    const regex = RegExp("/[a-zA-Z0-9]{6,12}/");
+
+    if (req.body.email == "" || req.body.email == undefined) {
+        errors.push("You must enter an email");
+    }
+
+    if (req.body.psw == "" || req.body.psw == undefined) {
+        errors.push("You must enter a password");
+    }
+
+    if ((req.body.psw.match(regex)) == false) {
+        errors.push("Password must be between 6-12 characters");
+    }
+
+
+    if (errors.length > 0) {
+        res.render("Register", {
+            title: "Register",
+            errorMessages: errors
+        })
+    } else {
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+        const msg = {
+            to: `${req.body.email}`,
+            from: "mishafrolov882000@gmail.com",
+            subject: 'Dinner Time Account Registration',
+            html: 'Thank you for registering with Dinner Time. Enjoy amazing food just by the tap of a few buttons!',
+        };
+
+        sgMail.send(msg)
+            .then(() => {
+                res.redirect("/");
+            })
+            .catch(err => {
+                console.log(`Error ${err}`);
+            })
+    }
+});
 
 module.exports = router;
